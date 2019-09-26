1bc02d84b1a0d1896fa8b9deb271cfff6e438adb
Ahmad Asaad <a.asaad@windowslive.com>
2019-09-26 21:16:56 +0300

    Now You Can Add Items To List

--- app/src/main/java/com/example/glowing_waffle/MainActivity.java
+++ app/src/main/java/com/example/glowing_waffle/MainActivity.java
@@ -1,35 +1,83 @@
 package com.example.glowing_waffle;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import libs.mjn.prettydialog.PrettyDialog;
 import libs.mjn.prettydialog.PrettyDialogCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView todoList;
+    private ArrayList<String> todoArray;
+    private ArrayAdapter todoAdapter;
+    private String ToDoText = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        todoList = findViewById(R.id.todoList);
+        todoArray = new ArrayList<>();
+        todoAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, todoArray);
+
+
         FloatingActionButton addItem = findViewById(R.id.add);
+
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Add To List");
+
+                // Set up the input
+                final EditText input = new EditText(MainActivity.this);
+
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ToDoText = input.getText().toString();
+
+                        todoArray.add(ToDoText);
+                        todoList.setAdapter(todoAdapter);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
             }
         });
     }
@@ -69,11 +117,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                     )
                     .show();
-
-        }else if(id == R.id.addItem) {
-            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
